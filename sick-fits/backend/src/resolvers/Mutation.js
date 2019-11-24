85e47fb6783a68c5034c08d82d21a031447d8d3c
Brian Chan <brianchan.sea@gmail.com>
2019-11-23 20:27:01 -0800

    Add a signout button

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -106,6 +106,11 @@ const Mutations = {
     // 5. Return the user.
     return user;
   },
+
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+    return { message: 'Goodbye!' };
+  }
 };
 
 module.exports = Mutations;
